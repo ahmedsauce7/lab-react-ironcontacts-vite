c01505177e742439476f6fe571694beabdee27c4
ahmed awad <ahmeedsauce7@gmail.com>
2023-05-10 01:14:42 +0300

    solved lab with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,61 +14,56 @@ function addRandom() {
   const addedCeleb = [...firstFive,newCeleb]
   console.log(newCeleb)
   setFirstFive(addedCeleb)
-}
+};
+
+function sortByPopularity(){
+  const popularceleb = [...firstFive].sort((a, b) => b.popularity - a.popularity)
+  setFirstFive(popularceleb)
+};
+
+function sortByName(){
+  const aplhaceleb = [...firstFive].sort((a, b) => a.name.localeCompare(b.name))
+  setFirstFive(aplhaceleb)
+};
+
+function deleteContact(id){
+  const deleteceleb = [...firstFive].filter((contact) => contact.id !== id)
+  setFirstFive(deleteceleb)
+};
+
 
   return ( 
   <div className='App'>
-    <h1>Iron Contacts</h1>
-    <button onClick={() => addRandom()}>Add Random Contact</button>
-  <table>
+    <h1 className='title'>Iron Contacts</h1>
+    <h1 className='title2'>🌕🌖🌗🌘🌑🌒🌓🌔</h1>
+    <button className='buttons' onClick={() => addRandom()}>Add Random Contact</button>
+    <button className='buttons' onClick={() => sortByPopularity()}>Sort by popularity</button>
+    <button className='buttons' onClick={() => sortByName()}>Sort by name</button>
+  <table className='table'>
     <thead>
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Poularity</th>
-        <th>Won Emmy</th>
-        <th>Won Oscar</th>
+      <tr >
+        <th className='table'>Picture</th>
+        <th className='table'>Name</th>
+        <th className='table'>Poularity</th>
+        <th className='table'>Won Emmy</th>
+        <th className='table'>Won Oscar</th>
+        <th className='table'>Action</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td><img className='celebimage' src={firstFive[0].pictureUrl} alt={firstFive[0].name}/></td>
-        <td>{firstFive[0].name}</td>
-        <td>{firstFive[0].popularity.toFixed(2)}</td>
-        <td>{firstFive[0].wonEmmy ? '🏆' : null}</td>
-        <td>{firstFive[0].wonOscar ? '🏆' : null}</td>
+      {firstFive.map((firstFive,index) => (
+        <tr key={index}>
+        <td className='table2'><img className='celebimage' src={firstFive.pictureUrl} alt={firstFive.name}/></td>
+        <td className='table2'>{firstFive.name}</td>
+        <td className='table2'>{firstFive.popularity.toFixed(2)}</td>
+        <td className='table2'>{firstFive.wonEmmy ? '🏆' : "💩"}</td>
+        <td className='table2'>{firstFive.wonOscar ? '🏆' : "💩"}</td>
+        <button className='deletebtn' onClick={() => deleteContact(firstFive.id)}></button>
       </tr>
-      <tr>
-        <td><img className='celebimage' src={firstFive[1].pictureUrl} alt={firstFive[1].name}/></td>
-        <td>{firstFive[1].name}</td>
-        <td>{firstFive[1].popularity.toFixed(2)}</td>
-        <td>{firstFive[1].wonEmmy ? '🏆' : null}</td>
-        <td>{firstFive[1].wonOscar ? '🏆' : null}</td>
-      </tr>
-      <tr>
-        <td><img className='celebimage' src={firstFive[2].pictureUrl} alt={firstFive[2].name}/></td>
-        <td>{firstFive[2].name}</td>
-        <td>{firstFive[2].popularity.toFixed(2)}</td>
-        <td>{firstFive[2].wonEmmy ? '🏆' : null}</td>
-        <td>{firstFive[2].wonOscar ? '🏆' : null}</td>
-      </tr>
-      <tr>
-        <td><img className='celebimage' src={firstFive[3].pictureUrl} alt={firstFive[4].name}/></td>
-        <td>{firstFive[3].name}</td>
-        <td>{firstFive[3].popularity.toFixed(2)}</td>
-        <td>{firstFive[3].wonEmmy ? '🏆' : null}</td>
-        <td>{firstFive[3].wonOscar ? '🏆' : null}</td>
-      </tr>
-      <tr>
-        <td><img className='celebimage' src={firstFive[4].pictureUrl} alt={firstFive[4].name}/></td>
-        <td>{firstFive[4].name}</td>
-        <td>{firstFive[4].popularity.toFixed(2)}</td>
-        <td>{firstFive[4].wonEmmy ? '🏆': null}</td>
-        <td>{firstFive[4].wonOscar ? '🏆': null}</td>
-      </tr>
+      ))}
     </tbody>
   </table>
   </div>
-  ) 
-}
+  ); 
+};
 export default App
