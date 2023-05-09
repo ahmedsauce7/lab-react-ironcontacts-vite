bd7c81b73335374ada51b8d0ab8d1ee7e1796bd8
ahmed awad <ahmeedsauce7@gmail.com>
2023-05-09 23:15:37 +0300

    trying a different way that manually adding the celebs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,74 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import contacts from "./contacts.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function addRandom() {
+  let afterFive = contacts.filter(oneContact => {
+    return !firstFive.includes(oneContact)
+  })
+  const randomCeleb = Math.floor(Math.random() * afterFive.length)
+  const newCeleb = afterFive[randomCeleb]
+  const addedCeleb = [...firstFive,newCeleb]
+  console.log(newCeleb)
+  setFirstFive(addedCeleb)
 }
 
+  return ( 
+  <div className='App'>
+    <h1>Iron Contacts</h1>
+    <button onClick={() => addRandom()}>Add Random Contact</button>
+  <table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Poularity</th>
+        <th>Won Emmy</th>
+        <th>Won Oscar</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><img className='celebimage' src={firstFive[0].pictureUrl} alt={firstFive[0].name}/></td>
+        <td>{firstFive[0].name}</td>
+        <td>{firstFive[0].popularity.toFixed(2)}</td>
+        <td>{firstFive[0].wonEmmy ? '🏆' : null}</td>
+        <td>{firstFive[0].wonOscar ? '🏆' : null}</td>
+      </tr>
+      <tr>
+        <td><img className='celebimage' src={firstFive[1].pictureUrl} alt={firstFive[1].name}/></td>
+        <td>{firstFive[1].name}</td>
+        <td>{firstFive[1].popularity.toFixed(2)}</td>
+        <td>{firstFive[1].wonEmmy ? '🏆' : null}</td>
+        <td>{firstFive[1].wonOscar ? '🏆' : null}</td>
+      </tr>
+      <tr>
+        <td><img className='celebimage' src={firstFive[2].pictureUrl} alt={firstFive[2].name}/></td>
+        <td>{firstFive[2].name}</td>
+        <td>{firstFive[2].popularity.toFixed(2)}</td>
+        <td>{firstFive[2].wonEmmy ? '🏆' : null}</td>
+        <td>{firstFive[2].wonOscar ? '🏆' : null}</td>
+      </tr>
+      <tr>
+        <td><img className='celebimage' src={firstFive[3].pictureUrl} alt={firstFive[4].name}/></td>
+        <td>{firstFive[3].name}</td>
+        <td>{firstFive[3].popularity.toFixed(2)}</td>
+        <td>{firstFive[3].wonEmmy ? '🏆' : null}</td>
+        <td>{firstFive[3].wonOscar ? '🏆' : null}</td>
+      </tr>
+      <tr>
+        <td><img className='celebimage' src={firstFive[4].pictureUrl} alt={firstFive[4].name}/></td>
+        <td>{firstFive[4].name}</td>
+        <td>{firstFive[4].popularity.toFixed(2)}</td>
+        <td>{firstFive[4].wonEmmy ? '🏆': null}</td>
+        <td>{firstFive[4].wonOscar ? '🏆': null}</td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+  ) 
+}
 export default App
